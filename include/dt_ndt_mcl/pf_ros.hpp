51a2502226952b8bef82bfdb9aec74ec3074ea2e
Roald Ong <roald.ong@gmail.com>
2024-05-03 16:10:57 +0800

    Add in load map callback

--- include/dt_ndt_mcl/pf_ros.hpp
+++ include/dt_ndt_mcl/pf_ros.hpp
@@ -1,18 +1,27 @@
 #ifndef PF_ROS_HPP_
 #define PF_ROS_HPP_
 
+#include <nav_msgs/OccupancyGrid.h>
 #include <ros/ros.h>
 
+#include <dt_ndt_mcl/particle_filter.hpp>
 #include <dt_ndt_mcl/scan_matcher_ndt.hpp>
+#include <iostream>
 
 class ParticleFilter2D {
  public:
   ParticleFilter2D(ros::NodeHandle &nh, ros::NodeHandle &pnh);
 
+  void mapCallback(const nav_msgs::OccupancyGrid::ConstPtr &msg);
+
  private:
   ndt_2d::ScanMatcherNDT m_scan_matcher;
+
   ros::NodeHandle &m_nh;
   ros::NodeHandle &m_prv_nh;
+  ros::Subscriber m_map_sub;
+
+  bool m_received_map;
 };
 
 #endif  // PF_ROS_HPP_
--- src/pf_ros.cpp
+++ src/pf_ros.cpp
@@ -3,4 +3,15 @@
 ParticleFilter2D::ParticleFilter2D(ros::NodeHandle& nh, ros::NodeHandle& pnh)
     : m_nh(nh), m_prv_nh(pnh) {
   m_scan_matcher.initialize("ndt", nh, 100.0);
+  m_map_sub =
+      m_prv_nh.subscribe("/map", 1, &ParticleFilter2D::mapCallback, this);
+
+  m_received_map = false;
+}
+
+void ParticleFilter2D::mapCallback(
+    const nav_msgs::OccupancyGrid::ConstPtr& msg) {
+  m_scan_matcher.addMap(*msg);
+  std::cout<<"Map received"<<std::endl;
+  m_received_map = true;
 }
